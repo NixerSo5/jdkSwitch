bd3e05b2551bc551049f2397ddef3e606bc00663
nixer <nixer456@163.com>
2023-09-20 16:13:11 +0800

    Add project files.

---
+++ jdkSwitch/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jdkSwitch
+{
+    public partial class Form1 : Form
+    {
+
+
+        string jdkpath = null;
+        string basepath = null; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string localpath = System.Windows.Forms.Application.StartupPath;
+            string dpath = localpath + "/jdkSwitchJdks";
+            if (!Directory.Exists(localpath + "/jdkSwitchJdks"))
+            {
+                Directory.CreateDirectory(dpath);
+            }
+            string[] subDirectories = Directory.GetDirectories(dpath);
+            basepath = localpath;
+            foreach (string subDirectory in subDirectories)
+            {
+                Console.WriteLine(subDirectory.Length);
+                Console.WriteLine(basepath.Length);
+                comboBox1.Items.Add(subDirectory.Substring(basepath.Length+14, subDirectory.Length-14-basepath.Length));
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          string jhome =   System.Environment.GetEnvironmentVariable("JAVA_HOME",EnvironmentVariableTarget.Machine);
+            if (jhome == "")
+            { //C:\Program Files\Java\jdk1.8.0_351
+              //开始安装jdk
+
+            }
+            else {
+                string pathStr = System.Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+                string[] pathArry = pathStr.Split(';');
+                ArrayList pathlist = new ArrayList(pathArry);
+                if (!pathlist.Contains("%JAVA_HOME%\\bin"))
+                {
+                    pathlist.Add("%JAVA_HOME%\\bin");
+                }
+                pathArry = (string[])pathlist.ToArray(typeof(string));
+                pathStr = string.Join(";", pathArry);
+                System.Environment.SetEnvironmentVariable("Path", pathStr, EnvironmentVariableTarget.Machine);
+
+                // 查看是否选中  如果选中   更新变量  如果没有  提示选中或者下载
+                System.Environment.SetEnvironmentVariable("JAVA_HOME",jdkpath, EnvironmentVariableTarget.Machine);
+                MessageBox.Show("设置成功");
+
+                // MessageBox.Show(localpath);
+            }
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            jdkpath = basepath+ "/jdkSwitchJdks/" + comboBox1.SelectedItem.ToString();
+         
+        }
+    }
+}
---
+++ jdkSwitch/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jdkSwitch
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+
+
+
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            //判断当前登录用户是否为管理员
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                //如果是管理员，则直接运行Application.Run(new FrmMain());
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else
+            {
+
+
+                MessageBox.Show("请使用管理员打开");
+                Application.Exit();
+
+
+            }
+        }
+    }
+}
