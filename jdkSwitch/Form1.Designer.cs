fad20f616c406d82d3dcd0317ca426969b8b1b10
nixer <nixer456@163.com>
2023-09-27 19:13:51 +0800

    加了下载jdk

--- jdkSwitch/Form1.Designer.cs
+++ jdkSwitch/Form1.Designer.cs
@@ -30,38 +30,106 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(230, 250);
+            this.button1.Location = new System.Drawing.Point(161, 213);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(315, 85);
+            this.button1.Size = new System.Drawing.Size(213, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "submit";
+            this.button1.Text = "更换版本";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(54, 97);
+            this.comboBox1.Location = new System.Drawing.Point(161, 97);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(262, 23);
+            this.comboBox1.Size = new System.Drawing.Size(213, 23);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选择jdk";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(476, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(213, 23);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(409, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "下载jdk";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(467, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "下载版本";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(161, 365);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(519, 23);
+            this.progressBar1.TabIndex = 6;
+         
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(161, 394);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(519, 23);
+            this.progressBar2.TabIndex = 7;
+          
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +137,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
     }
 }
 
--- jdkSwitch/Form1.cs
+++ jdkSwitch/Form1.cs
@@ -2,31 +2,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO.Compression;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ionic.Zip;
+using System.Windows;
+
 
 namespace jdkSwitch
 {
     public partial class Form1 : Form
     {
-
-
+        string zipfilepath = null;
+        string zipfilename = null;
         string jdkpath = null;
-        string basepath = null; 
+        string basepath = null;
+
+        private WebClient client;
+
 
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            creatSelectBox1();
+
+            String[] downloadf = new string[] { "21", "17", "11", "8" };
+            foreach (string jdk in downloadf)
+            {
+                comboBox2.Items.Add(jdk);
+            }
+
+        }
+
+        private void creatSelectBox1()
+        {
+            comboBox1.Items.Clear();
             string localpath = System.Windows.Forms.Application.StartupPath;
             string dpath = localpath + "/jdkSwitchJdks";
             if (!Directory.Exists(localpath + "/jdkSwitchJdks"))
@@ -39,44 +61,182 @@ namespace jdkSwitch
             {
                 Console.WriteLine(subDirectory.Length);
                 Console.WriteLine(basepath.Length);
-                comboBox1.Items.Add(subDirectory.Substring(basepath.Length+14, subDirectory.Length-14-basepath.Length));
+                comboBox1.Items.Add(subDirectory.Substring(basepath.Length + 14, subDirectory.Length - 14 - basepath.Length));
 
             }
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-          string jhome =   System.Environment.GetEnvironmentVariable("JAVA_HOME",EnvironmentVariableTarget.Machine);
-            if (jhome == "")
-            { //C:\Program Files\Java\jdk1.8.0_351
-              //开始安装jdk
+            if (comboBox1.SelectedItem != null)
+            {
 
-            }
-            else {
-                string pathStr = System.Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
-                string[] pathArry = pathStr.Split(';');
-                ArrayList pathlist = new ArrayList(pathArry);
-                if (!pathlist.Contains("%JAVA_HOME%\\bin"))
-                {
-                    pathlist.Add("%JAVA_HOME%\\bin");
+                string jhome = System.Environment.GetEnvironmentVariable("JAVA_HOME", EnvironmentVariableTarget.Machine);
+                if (jhome == "")
+                { //C:\Program Files\Java\jdk1.8.0_351
+                  //开始安装jdk
+
                 }
-                pathArry = (string[])pathlist.ToArray(typeof(string));
-                pathStr = string.Join(";", pathArry);
-                System.Environment.SetEnvironmentVariable("Path", pathStr, EnvironmentVariableTarget.Machine);
+                else
+                {
+                    string pathStr = System.Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+                    string[] pathArry = pathStr.Split(';');
+                    ArrayList pathlist = new ArrayList(pathArry);
+                    if (!pathlist.Contains("%JAVA_HOME%\\bin"))
+                    {
+                        pathlist.Add("%JAVA_HOME%\\bin");
+                    }
+                    pathArry = (string[])pathlist.ToArray(typeof(string));
+                    pathStr = string.Join(";", pathArry);
+                    System.Environment.SetEnvironmentVariable("Path", pathStr, EnvironmentVariableTarget.Machine);
 
-                // 查看是否选中  如果选中   更新变量  如果没有  提示选中或者下载
-                System.Environment.SetEnvironmentVariable("JAVA_HOME",jdkpath, EnvironmentVariableTarget.Machine);
-                MessageBox.Show("设置成功");
+                    // 查看是否选中  如果选中   更新变量  如果没有  提示选中或者下载
+                    System.Environment.SetEnvironmentVariable("JAVA_HOME", jdkpath, EnvironmentVariableTarget.Machine);
+                    MessageBox.Show("设置成功");
 
-                // MessageBox.Show(localpath);
+                    // MessageBox.Show(localpath);
+                }
             }
-          
+            else
+            {
+                MessageBox.Show("没选择jdk");
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            jdkpath = basepath+ "/jdkSwitchJdks/" + comboBox1.SelectedItem.ToString();
-         
+            jdkpath = basepath + "/jdkSwitchJdks" + comboBox1.SelectedItem.ToString();
+
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void WebClientDownloadCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show($"文件下载失败：{e.Error.Message}");
+            }
+            else
+            {
+                MessageBox.Show("文件下载成功！");
+                UnZipJdk(zipfilepath, zipfilename);
+
+            }
+
+            client.Dispose();
+        }
+
+        private void UnZipJdk(string zipPath, string extractPath)
+        {
+            string localpath = System.Windows.Forms.Application.StartupPath;
+            string dpath = localpath + "/jdkSwitchJdks";
+            extractPath = dpath;
+
+            using (ZipFile zipFile = ZipFile.Read(zipPath))
+            {
+                zipFile.ExtractProgress += ZipFile_ExtractProgress;
+
+                foreach (ZipEntry entry in zipFile)
+                {
+                    if (entry.IsDirectory)
+                    {
+                        string entryFileName = entry.FileName;
+                        string fullZipToPath = Path.Combine(extractPath, entryFileName);
+                        string directoryName = Path.GetDirectoryName(fullZipToPath);
+
+                        if (directoryName.Length > 0)
+                        {
+                            Directory.CreateDirectory(directoryName);
+                        }
+
+
+
+
+
+                    }
+                    else { entry.Extract(extractPath, ExtractExistingFileAction.OverwriteSilently); }
+
+
+
+
+
+                }
+            }
+
+
+            MessageBox.Show("Extraction complete!");
+            creatSelectBox1();
+        }
+
+
+
+
+
+
+
+        private void ZipFile_ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            if (e.EventType == ZipProgressEventType.Extracting_EntryBytesWritten)
+            {
+                int progress = (int)((e.BytesTransferred * 100) / e.TotalBytesToTransfer);
+                progressBar2.Value = progress;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != null)
+            {
+                String[] downloadlist = new string[] { "https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_windows-x64_bin.zip"
+                , "https://download.java.net/openjdk/jdk17/ri/openjdk-17+35_windows-x64_bin.zip",
+                "https://download.java.net/openjdk/jdk11.0.0.1/ri/openjdk-11.0.0.1_windows-x64_bin.zip",
+                "https://download.java.net/openjdk/jdk8u43/ri/openjdk-8u43-windows-i586.zip"};
+                //chat to int
+                int index = comboBox2.SelectedIndex;
+                String[] namelist = new string[] { "jdk21.zip", "jdk17.zip", "jdk11.zip", "jdk8.zip" };
+
+                string fileUrl = downloadlist[index];
+                string localpath = System.Windows.Forms.Application.StartupPath;
+                string savePath = localpath + "\\jdkSwitchJdks\\" + namelist[index];
+                zipfilepath = savePath;
+                client = new WebClient();
+                client.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                client.DownloadFileCompleted += WebClientDownloadCompleted;
+
+
+                try
+                {
+                    client.DownloadFileAsync(new Uri(fileUrl), @savePath);
+                //  UnZipJdk(zipfilepath, zipfilename);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"文件下载失败：{ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("没有选择要下载的版本!");
+            }
+
+        }
+
     }
 }
